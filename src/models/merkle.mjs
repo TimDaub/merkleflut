5cc629936462205dd06f00c3a1e0649b47f96ad7
Tim Daubenschütz <tim@daubenschuetz.de>
2021-05-21 15:49:31 +0200

    Implement hash tree serialization

--- src/models/merkle.mjs
+++ src/models/merkle.mjs
@@ -1,22 +1,62 @@
 // @format
 import { Keccak } from "sha3";
 
+const separators = {
+  branch: "/",
+  fragment: ":"
+};
+
 export class HashTreeNode {
   constructor(root, data) {
     this.root = root;
     this.data = data;
 
-    if (root) {
+    if (root && root.hash) {
       this.hash = gethash(`${root.hash}:${data}`);
     } else {
       this.hash = gethash(data);
     }
   }
+
+  serialize() {
+    let root = null;
+    if (this.root && this.root.hash) {
+      root = this.root.hash;
+    }
+    return `${root}${separators.fragment}${this.data}${separators.fragment}${
+      this.hash
+    }`;
+  }
+}
+
+export function serialize(leaf, path = []) {
+  path.push(leaf.serialize());
+  if (leaf.root) {
+    return serialize(leaf.root, path);
+  } else {
+    return path.reverse().join(separators.branch);
+  }
+}
+
+export function build(path, parentNode) {
+  if (path.length === 0) {
+    return parentNode;
+  }
+
+  const branches = path.split(separators.branch);
+  const [root, data, hash] = branches[0].split(separators.fragment);
+  branches.shift();
+  path = branches.join(separators.branch);
+
+  if (parentNode) {
+    return build(path, new HashTreeNode(parentNode, data, hash));
+  } else {
+    return build(path, new HashTreeNode(null, data, hash));
+  }
 }
 
 export function extract(leaf, sequence = []) {
   sequence.push(leaf.data);
-
   if (leaf.root) {
     return extract(leaf.root, sequence);
   } else {
--- test/models/merkle_test.mjs
+++ test/models/merkle_test.mjs
@@ -1,6 +1,12 @@
 // @format
 import test from "ava";
-import { gethash, HashTreeNode, extract } from "../../src/models/merkle.mjs";
+import {
+  serialize,
+  gethash,
+  HashTreeNode,
+  extract,
+  build
+} from "../../src/models/merkle.mjs";
 
 test("test if getting hash works", t => {
   const inp = "hello";
@@ -28,4 +34,40 @@ test("if serializing a hash tree works", t => {
   t.deepEqual(extract(branch1), ["hello", "world"]);
   t.deepEqual(extract(branch2), ["hello", "world", "merkle tree"]);
   t.deepEqual(extract(branch3), ["hello", "alternative universe"]);
+});
+
+test("serializing a hash tree node", t => {
+  const tree = new HashTreeNode(null, "hello");
+  t.is(
+    tree.serialize(),
+    "null:hello:1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8"
+  );
+
+  const branch1 = new HashTreeNode(tree, "world");
+  t.is(
+    branch1.serialize(),
+    "1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8:world:400b9d0185590d56409028ee32dc58bfa7179a94940508cff983930f93031f2e"
+  );
+});
+
+test("serializing a tree branch", t => {
+  const tree = new HashTreeNode(null, "hello");
+  const branch1 = new HashTreeNode(tree, "world");
+
+  const path = serialize(branch1);
+  t.is(
+    path,
+    "null:hello:1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8/1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8:world:400b9d0185590d56409028ee32dc58bfa7179a94940508cff983930f93031f2e"
+  );
+});
+
+test("building from a serialized tree", t => {
+  const sTree =
+    "null:hello:1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8/1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8:world:400b9d0185590d56409028ee32dc58bfa7179a94940508cff983930f93031f2e";
+  const tree = new HashTreeNode(null, "hello");
+  const branch1 = new HashTreeNode(tree, "world");
+
+  const leaf = build(sTree);
+  t.deepEqual(leaf, branch1);
+  t.is(branch1.hash, leaf.hash);
 });
